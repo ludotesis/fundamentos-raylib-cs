8d451acebd6dca8b9ab3ebeb0164ebd615072d14
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-04-09 19:59:12 -0300

    Dibujar Circulo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Raylib_cs;
+using System.Numerics;
+
+Vector2 position = new Vector2(100,500);
 
 Raylib.InitWindow(800, 480, "Hello World");
 
@@ -7,6 +10,8 @@ while (!Raylib.WindowShouldClose())
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.White);
     Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+    if (Raylib.IsKeyDown(KeyboardKey.Space)) position.Y -= 3;
+    Raylib.DrawCircle((int)position.X,(int)position.Y, 50, Color.Blue);
     Raylib.EndDrawing();
 }
 
