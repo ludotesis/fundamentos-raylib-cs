de9116ea54328505a8aabe001becf9180b6b9f53
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-04-09 19:31:00 -0300

    Interfaz Inicial
    Raylib.InitWindow
    Raylib.BeginDrawing
    Raylib.EndDrawing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 480, "Hello World");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
