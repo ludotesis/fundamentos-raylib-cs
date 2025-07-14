43b17a1a190e0ecacafe7ee1c556b727271cf519
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-07-14 15:14:06 -0300

    Clase Proyectil

---
+++ Proyectil.cs
@@ -0,0 +1,38 @@
+using Raylib_cs;                  
+using System.Numerics;
+
+class Proyectil
+{
+    Texture2D sprite;
+    Vector2 posicion;
+    public Rectangle hitbox;
+
+    float velocidad;
+    bool activado;
+
+    public Proyectil(float velocidad)
+    {
+        activado = false;
+        this.velocidad = velocidad;
+    }
+
+    public void IniciarProyectil(float posicionInicialX, float posicionInicialY)
+    {
+        posicion.X = posicionInicialX;
+        posicion.Y = posicionInicialY;
+        activado = true;
+    }
+
+    public void CargarSprite()
+    {
+        sprite = Raylib.LoadTexture("Proyectil.png");
+        hitbox = new Rectangle(posicion, sprite.Width, sprite.Height);
+    }
+    
+    public void Dibujar()
+    {
+        if (!activado) return;
+
+        Raylib.DrawTextureV(sprite, posicion, Color.White);
+    }
+}
