b3d88b6e3f04ae148ad6f1fe6eed612c6a6bba08
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-07-14 10:51:20 -0300

    Clase Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Introducción Raylib + C#");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Subscribite", 12, 12, 60, Color.Red);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
